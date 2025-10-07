1de848d33c2183255179fb4eac3ac84991acf7f4
cramptjoel <cramptjoel@myvuw.ac.nz>
2025-10-08 10:50:27 +1300

    Refactored MongoDB connection to use await

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PASSWORD = encodeURIComponent(process.env.PASSWORD);
 const MONGO_URI = `mongodb+srv://admin:${PASSWORD}@prod-checklist-asiasout.g2ssiy7.mongodb.net/checklist?retryWrites=true&w=majority&appName=prod-checklist-asiasoutheast2`;
 
 // MongoDB Connection
-mongoose.connect(MONGO_URI)
+await mongoose.connect(MONGO_URI)
 .then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
